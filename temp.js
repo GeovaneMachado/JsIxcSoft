018652cc4dbc5eacf6499f1ac5d94301c9b642aa
GeovaneMachado <73132514+GeovaneMachado@users.noreply.github.com>
2022-03-12 11:45:29 -0300

    Add files via upload
    temporizador

---
+++ temp.js
@@ -0,0 +1,38 @@
+function init(){
+    function BoasVindas(){
+        let divBemvindo = document.createElement('div');
+        let ola = document.createElement("h1");
+        document.body.appendChild(divBemvindo);
+        divBemvindo.appendChild(ola);
+        divBemvindo.style.display = 'inline-block';
+        divBemvindo.style.width = '400px';
+        divBemvindo.style.height = '200px';
+        divBemvindo.style.textAlign = 'center';
+        divBemvindo.style.backgroundColor = 'red';
+        ola.innerText = 'Bem vindo!!';
+        setTimeout(()=>{
+            document.body.removeChild(divBemvindo);
+            let paragrafo = document.getElementById('texto')
+            paragrafo.style.visibility = 'visible'
+        },3000)
+    }
+    BoasVindas();
+    function tempo(){
+        let time = Date.now();
+        var temp = setInterval(()=>{
+            let tempoAtual = Date.now();
+            let elapsedTime = Math.floor((tempoAtual - time))/1000;
+            time += 0.01;
+            document.getElementById('tempo').innerText = elapsedTime.toFixed(2);
+        }, 10);
+        let btn = document.getElementById('parar');
+        btn.addEventListener('click', ()=>{clearInterval(temp)})
+    }
+
+
+    let iniciar = document.getElementById('iniciar');
+    iniciar.addEventListener('click', tempo)
+    
+}
+
+window.addEventListener('load', init);
