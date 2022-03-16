3e9915e4989d90f75ff9d887e2a50f5920bc3275
GeovaneMachado <geovanemachado9@gmail.com>
2022-03-16 07:27:09 -0300

    Alterando perguntas no evento click

--- DesafioFInal/perguntas.js
+++ DesafioFInal/perguntas.js
@@ -1,27 +1,27 @@
 function init(){
 
     let respostas = {
-        
+        pergunta: 1,
+        resposta: 3,
     }
+    let perguntaAtual = 0;
+    let perguntas = document.getElementsByClassName("perguntas");
+    perguntas[perguntaAtual].style.display = "block";
     function escrevePergunta(){
-        let Perguntas = document.getElementsByClassName("perguntas");
-        let resposta = document.getElementsByClassName("respostaPergunta1");
-        for(let i=0; i<Perguntas.length; i++){
-            Perguntas[i].style.display ='block';
-            for(let j=0; j<resposta.length; j++){
-                resposta[2].addEventListener("click", ()=>{
-                    let numeroPergunta = document.getElementsByClassName("numeroPergunta");
-                    numeroPergunta[0].style.backgroundColor = "green";
-                    Perguntas[i].style.display ='none';
-                })
-            }
-        }
-            
+        perguntas[perguntaAtual].style.display = "block";
+        if(perguntaAtual == 5) return;
+        perguntas[perguntaAtual].addEventListener("click", ()=>{
+            perguntas[perguntaAtual].style.display = "none";
+            perguntaAtual++;
+            escrevePergunta();           
+        });
     }
-    escrevePergunta()
- 
-    function resposta(){
+    escrevePergunta();
+/*     function resposta(){
+        let respostaCorreta = respostas.resposta;
+        let idResposta =document.getElementById(resposta.resposta);
+        if()
         return;
-    }
-}
+    }*/
+} 
 window.addEventListener("load", init)
