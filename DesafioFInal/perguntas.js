d11c495c031784aa3745904b80eee5b124526006
GeovaneMachado <geovanemachado9@gmail.com>
2022-03-18 13:14:52 -0300

    click em tbody

--- DesafioFInal/perguntas.js
+++ DesafioFInal/perguntas.js
@@ -1,22 +1,40 @@
 function init(){
 
-    let respostas = {
-        pergunta: 1,
-        resposta: 3,
-    }
+    let respostas = [
+        {
+            pergunta: document.getElementById("pergunta1"),
+            resposta: 3,
+            corpResp: document.getElementById("1"),
+        },
+        {
+            pergunta: document.getElementById("pergunta2"),
+            resposta: 3,
+            corpResp: document.getElementById("2"),
+        },
+        {
+            pergunta: document.getElementById("pergunta3"),
+            resposta: 3,
+        }
+    ]
     let perguntaAtual = 0;
     let perguntas = document.getElementsByClassName("perguntas");
-    perguntas[perguntaAtual].style.display = "block";
+    let resp = document.getElementsByClassName("resp");
+    debugger
     function escrevePergunta(){
-        perguntas[perguntaAtual].style.display = "block";
+        debugger
+        respostas[perguntaAtual].pergunta.style.display = "block";
         if(perguntaAtual == 5) return;
-        perguntas[perguntaAtual].addEventListener("click", ()=>{
-            perguntas[perguntaAtual].style.display = "none";
+        respostas[perguntaAtual].corpResp.addEventListener("click",()=>{
+            respostas[perguntaAtual].pergunta.style.display = "none";
             perguntaAtual++;
-            escrevePergunta();           
+            escrevePergunta();   
         });
     }
+    debugger
     escrevePergunta();
+    //let element = perguntas[perguntaAtual].TagName("tbody");
+    //element.addEventListener("click", proxima)
+           
 /*     function resposta(){
         let respostaCorreta = respostas.resposta;
         let idResposta =document.getElementById(resposta.resposta);
